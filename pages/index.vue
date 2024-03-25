5142458a32ba8aa7f721481263b437321e9cd29f
Mateus Gomes <mateus.correia@assert.ifpb.edu.br>
2024-03-25 14:57:03 -0300

    add tailwindcss

---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <h1>Welcome to the homepage</h1>
+    <NuxtLink to="/login">
+        <button type="button" class="bg-blue-500 px-6 pb-2">
+            Login
+        </button>
+    </NuxtLink>
+  </div>
+</template>
---
+++ pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1>Welcome to the login page</h1>
+    <CardLogin>
+      This is an auto-imported component.
+      <NuxtLink to="/" class="bg-red-300">Index</NuxtLink>
+    </CardLogin>
+  </div>
+</template>
+
+<script setup>
+import CardLogin from '~/components/CardLogin.vue';
+</script>
