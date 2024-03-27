08376e61d46be9d452cc95d7ea77d1412f49257d
Marcela Kramer <marcelabarretokramer10@gmail.com>
2024-03-27 10:30:39 -0300

    use fetch

--- layouts/custom.vue
+++ layouts/custom.vue
@@ -1,6 +1,42 @@
 <template>
-    <div>
-        <p>custom layout here!</p>
-        <slot />
+    <div class="card">
+        <Menubar :model="items">
+            <template #item="{ item, props, hasSubmenu }">
+                <router-link v-if="item.route" v-slot="{ href, navigate }" :to="item.route" custom>
+                    <a v-ripple :href="href" v-bind="props.action" @click="navigate">
+                        <span :class="item.icon" />
+                        <span class="ml-2">{{ item.label }}</span>
+                    </a>
+                </router-link>
+                <a v-else v-ripple :href="item.url" :target="item.target" v-bind="props.action">
+                    <span :class="item.icon" />
+                    <span class="ml-2">{{ item.label }}</span>
+                    <span v-if="hasSubmenu" class="pi pi-fw pi-angle-down ml-2" />
+                </a>
+            </template>
+        </Menubar>
     </div>
+    <slot />
 </template>
+
+<script setup>
+import { ref } from "vue";
+
+const items = ref([
+    {
+        label: 'Home',
+        icon: 'pi pi-home',
+        route: '/'
+    },
+    {
+        label: 'Login',
+        icon: 'pi pi-user',
+        route: '/login'
+    },
+    {
+        label: 'Products',
+        icon: 'pi pi pi-pencil',
+        route: '/products'
+    },
+]);
+</script>
---
+++ pages/products/[id].vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <p>Product details for id: {{ id }}</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dignissimos maiores fugit deleniti odio sint, vitae vel totam excepturi! Quas cupiditate optio vel fuga ratione necessitatibus cum eos modi explicabo nobis.</p>
+    </div>
+    <NuxtLink to="/products"><Button label="voltar"/></NuxtLink>
+</template>
+
+<script setup>
+    import { ref } from "vue";
+
+    const route = useRoute();
+    const id = ref(route.params.id);
+
+    const uri = "https://fakestoreapi.com/products/" + id
+    const { data : products } = await useFetch()
+
+</script>
